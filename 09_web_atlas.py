5eee2450453bddc661bb6b4987cb28576d877f41
Regan White <reganwhite@Regans-MacBook-Air-2.local>
2026-05-04 13:08:58 -0400

    Add deployment files for Render

--- 09_web_atlas.py
+++ 09_web_atlas.py
@@ -230,3 +230,4 @@ def update_guidelines(drug):
 if __name__ == '__main__':
     print("\n🌐 Server running at http://127.0.0.1:8050")
     app.run(debug=False, host='0.0.0.0', port=8050)
+server = app.server
